757d9a086ea5d2834880186f049d8801c875d1c8
RashaSayed <rasha.sayed@outlook.com>
2018-11-23 11:48:31 +0200

    second

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
 import React from 'react'
-import { Switch, Route } from 'react-router-dom';
+import * as BooksAPI from './api/BooksAPI'
+import './styles/App.css'
+import BookShelfList from "./components/BookShelfList";
+import SearchBook from "./components/SearchBook";
+//import {Route} from 'react-router-dom';
+import { Route } from 'react-router-dom';
+export default class BooksApp extends React.Component {
+    constructor(args) {
+        super(args);
+        this.state = {
+            books: [],
+            loading: true,
+        }
+    }
 
-import  Home  from './views/Home';
-import  Search  from './views/Search';
-import  Provider, {MyContext} from './Provider';
-import './css/App.css'
+    componentDidMount() {
+        BooksAPI.getAll().then(books => {
+            this.setState({books: books, loading: false})
+        })
+    }
 
+    onShelfChange = (book, shelf) => {
+        BooksAPI.update(book, shelf)
+            .then(
+                this.setState((state) => ({
+                    books: state.books.map(b => {
+                        if (b.title === book.title) {
+                            b.shelf = shelf;
+                            return b
+                        } else {
+                            return b
+                        }
+                    }),
+                    loading: false
+                }))
+            )
+    };
 
-class BooksApp extends React.Component {
+    render() {
+        const state = this.state;
+        const currentlyReading = state.books.filter((book) => book.shelf === 'currentlyReading')
+        const wantToRead = state.books.filter((book) => book.shelf === 'wantToRead')
+        const read = state.books.filter((book) => book.shelf === 'read')
 
-  render() {
-    return (
-      <div className="app">
-      <Provider>  
-          <Switch>
-              <Route exact path={"/"} render={() => <MyContext.Consumer>{context => <Home {...context} /> }</MyContext.Consumer>} />
-              <Route exact path={"/search"}  render={() => <MyContext.Consumer>{context => <Search {...context} /> }</MyContext.Consumer>} />
-          </Switch>
-        </Provider>
-      </div>
-    )
-  }
+        return (
+            <div className="app">
+                <Route path="/" exact render={() => (
+                    <div>
+                        <div className="list-books-title">
+                            <h1>myReads: Your Personal Library</h1>
+                        </div>
+                        {
+                            !state.loading ? (
+                                <BookShelfList
+                                    currentlyReading={currentlyReading}
+                                    wantToRead={wantToRead}
+                                    read={read}
+                                    onShelfChange={this.onShelfChange}
+                                />
+                            ) : (
+                                <div className="loader"/>
+                            )
+                        }
+                    </div>
+                )}/>
+                <Route path="/search" render={({history}) => (
+                    <SearchBook
+                        onShelfChange={this.onShelfChange}
+                        history={history}
+                        books={currentlyReading.concat(wantToRead, read)}
+                    />
+                )}/>
+            </div>
+        )
+    }
 }
-
-export default BooksApp
--- src/components/Book.js
+++ src/components/Book.js
@@ -1,51 +1,35 @@
-import React, { Component } from 'react'
-import { update } from '../api/BooksAPI';
-// import { Shelf } from './Shelf';
 
-export class Book extends Component {
+import React, {Component} from 'react';
 
-
-  handlechange = async e => {
-      // console.log(e.target.value)
-      e.persist();
-      try {
+export default class Book extends Component {
+    onBookShelfChange = (e) => {
         const shelf = e.target.value;
-        const book = this.props;
-        const result  = await update (book,shelf)
-        // this.props.shelf = e.target.value;
-        this.props.moveBook(book,shelf,result);
-        // console.log(shelfStatusUpdate)
-      }catch (error){
-        console.log(error)
-      }
-    }
+        this.props.onShelfChange(this.props.book, shelf);
+    };
 
     render() {
-    return (
-        <li>
-                        <div className="book">
-                          <div className="book-top">
-                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${
-                              this.props.imageLinks ? this.props.imageLinks.thumbnail : ""                              
-                              })`}}>
-                              
-                              </div>
-                            <div className="book-shelf-changer">
-                              <select onChange={this.handlechange} value={this.props.shelf}>
-                                <option value="move" disabled>Move to...</option>
-                                <option value="currentlyReading">Currently Reading</option>
-                                <option value="wantToRead">Want to Read</option>
-                                <option value="read">Read</option>
-                                <option value="none">None</option>
-                              </select>
-                            </div>
-                          </div>
-                          <div className="book-title">{this.props.title ? this.props.title : "Title not available"}</div>
-                          <div className="book-authors">{this.props.authors ? this.props.authors[0] : "Author name not available"}</div>
-                        </div>
-                      </li>
-    )
-  }
-}
+        const {book} = this.props;
 
-export default Book
+        return (
+            <div className="book">
+                <div className="book-top">
+                    <div className="book-cover" style={{
+                        width: 128,
+                        height: 193,
+                        backgroundImage: `url("${ book.imageLinks.thumbnail }")`
+                    }}/>
+                    <div className="book-shelf-changer">
+                        <select onChange={this.onBookShelfChange} defaultValue={book.shelf}>
+                            <option value="none" disabled>Move to...</option>
+                            <option value="currentlyReading">Currently Reading</option>
+                            <option value="wantToRead">Want to Read</option>
+                            <option value="read">Read</option>
+                            <option value="none">None</option>
+                        </select>
+                    </div>
+                </div>
+                <div className="book-title">{book.title}</div>
+                <div className="book-authors">{book.authors}</div>
+            </div>);
+    }
+}
